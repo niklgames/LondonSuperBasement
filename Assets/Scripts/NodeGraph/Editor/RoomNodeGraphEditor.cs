102bfacf69bf0fe060948f5aed80d517015604ec
Chris Williams <chris0n0williams@gmail.com>
2022-08-03 17:05:29 +0100

    Created a simple Editor Window (Section 5, Lecture 8)

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,9 +5,41 @@ using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
+    private GUIStyle roomNodeStyle;
+
+    // Node layout values
+    private const float nodeWidth = 160f;
+    private const float nodeHeight = 75f;
+    private const int nodePadding = 25;
+    private const int nodeBorder = 12;
+
     [MenuItem("Room Node graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
     private static void OpenWindow()
     {
         GetWindow<RoomNodeGraphEditor>("Room Node Editor Graph");
+    }
+
+    private void OnEnable()
+    {
+        // Define node layout style
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node1") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+    }
+
+    /// <summary>
+    /// Draw Editor GUI
+    /// </summary>
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Label 1");
+        GUILayout.EndArea();
+        
+        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Label 2");
+        GUILayout.EndArea();
     }
 }
