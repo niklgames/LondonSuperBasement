0d6aa1fb827064aacc76fff20954d726afeae761
Chris Williams <chris0n0williams@gmail.com>
2023-06-30 14:31:15 +0100

    Settings.cs
    Converting our Animator string references to ints for a Hash lookup as its quicker than a string comparison

--- Assets/Scripts/Misc/Settings.cs
+++ Assets/Scripts/Misc/Settings.cs
@@ -16,5 +16,15 @@ public static class Settings
                                             // to fit together.
     #endregion ROOM SETTINGS
 
-
+    #region ANIMATOR PARAMETERS
+    // Animator parameters - Player
+    public static int aimUp = Animator.StringToHash("aimUp");
+    public static int aimDown = Animator.StringToHash("aimDown");
+    public static int aimUpRight = Animator.StringToHash("aimUpRight");
+    public static int aimUpLeft = Animator.StringToHash("aimUpLeft");
+    public static int aimRight = Animator.StringToHash("aimRight");
+    public static int aimLeft = Animator.StringToHash("aimLeft");
+    public static int isIdle = Animator.StringToHash("isIdle");
+    public static int isMoving = Animator.StringToHash("isMoving");
+    #endregion
 }
