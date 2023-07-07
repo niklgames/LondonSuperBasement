a55784c6d100a7271915e9c117bb8dcbf46bfdc1
Chris Williams <chris0n0williams@gmail.com>
2023-07-07 13:10:23 +0100

    AnimatePlayer.cs
    We cache a ref to Player.
    Subscribe and unsubscribe from the OnIdle and OnAimWeapon event.
    We set the Idle animation parameters
    We initialise the aim weapon animation parameters and then set them with a switch statement.

---
+++ Assets/Scripts/Player/AnimatePlayer.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+[DisallowMultipleComponent]
+public class AnimatePlayer : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        // Load components
+        player = GetComponent<Player>();
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to idle event
+        player.idleEvent.OnIdle += IdleEvent_OnIdle;
+
+        // Subscribe to weaponAim event
+        player.aimWeaponEvent.OnWeaponAim += AimWeaponEvent_OnWeaponAim;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the idle event
+        player.idleEvent.OnIdle -= IdleEvent_OnIdle;
+
+        // Unsubscribe from the weaponAim event
+        player.aimWeaponEvent.OnWeaponAim -= AimWeaponEvent_OnWeaponAim;
+    }
+
+    /// <summary>
+    /// On idle event handler
+    /// </summary>
+    private void IdleEvent_OnIdle(IdleEvent idleEvent)
+    {
+        SetIdleAnimationParameters();
+    }
+
+    /// <summary>
+    /// On weapon aim event handler
+    /// </summary>
+    private void AimWeaponEvent_OnWeaponAim(AimWeaponEvent aimWeaponEvent, AimWeaponEventArgs aimWeaponEventArgs)
+    {
+        InitializeAimAnimationParameters();
+
+        SetAimWeaponAnimationParameters(aimWeaponEventArgs.aimDirection);
+    }
+
+    /// <summary>
+    /// Initialize aim animation parameters
+    /// </summary>
+    private void InitializeAimAnimationParameters()
+    {
+        player.animator.SetBool(Settings.aimUp, false);
+        player.animator.SetBool(Settings.aimUpRight, false);
+        player.animator.SetBool(Settings.aimUpLeft, false);
+        player.animator.SetBool(Settings.aimRight, false);
+        player.animator.SetBool(Settings.aimLeft, false);
+        player.animator.SetBool(Settings.aimDown, false);
+    }
+
+    /// <summary>
+    /// Set movement animation parameters
+    /// </summary>
+    private void SetIdleAnimationParameters()
+    {
+        player.animator.SetBool(Settings.isMoving, false);
+        player.animator.SetBool(Settings.isIdle, true);
+    }
+
+    /// <summary>
+    /// Set aim weapon animation parameters
+    /// </summary>
+    private void SetAimWeaponAnimationParameters(AimDirection aimDirection)
+    {
+        // Set aim direction
+        switch (aimDirection)
+        {
+            case AimDirection.Up:
+                player.animator.SetBool(Settings.aimUp, true);
+                break;
+
+            case AimDirection.UpRight:
+                player.animator.SetBool(Settings.aimUpRight, true);
+                break;
+
+            case AimDirection.UpLeft:
+                player.animator.SetBool(Settings.aimUpLeft, true);
+                break;
+
+            case AimDirection.Right:
+                player.animator.SetBool(Settings.aimRight, true);
+                break;
+
+            case AimDirection.Left:
+                player.animator.SetBool(Settings.aimLeft, true);
+                break;
+
+            case AimDirection.Down:
+                player.animator.SetBool(Settings.aimDown, true);
+                break;
+
+        }
+    }
+}
