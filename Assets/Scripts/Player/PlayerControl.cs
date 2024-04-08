18132b426992f8319f60906377d29b51ffc10bbd
Chris Williams <chris0n0williams@gmail.com>
2024-04-08 15:08:01 +0100

    Player Control script location change

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    #region Tooltip
+
+    [Tooltip("The player WeaponShootPosition gameobject in the heirarchy")]
+
+    #endregion
+
+    [SerializeField] private Transform weaponShootPosition;
+
+    private Player player;
+
+    private void Awake()
+    {
+        // Load components
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        // Process the player movement input
+        MovementInput();
+
+        // Process the player weapon input
+        WeaponInput();
+    }
+
+    /// <summary>
+    /// Player movement input
+    /// </summary>
+    private void MovementInput()
+    {
+        player.idleEvent.CallIdleEvent();
+    }
+
+    /// <summary>
+    /// Weapon Input  
+    /// </summary>
+    private void WeaponInput()
+    {
+        Vector3 weaponDirection;
+        float weaponAngleDegrees, playerAngleDegrees;
+        AimDirection playerAimDirection;
+
+        AimWeaponInput(out weaponDirection, out weaponAngleDegrees, out playerAngleDegrees, out playerAimDirection);
+    }
+
+    private void AimWeaponInput(out Vector3 weaponDirection, out float weaponAngleDegrees, out float playerAngleDegrees, out AimDirection playerAimDirection)
+    {
+        // Get mouse world position
+        Vector3 mouseWorldPosition = HelperUtilities.GetMouseWorldPosition();
+
+        // Calculate direction vector of mouse cursor from weapon shoot position
+        weaponDirection = (mouseWorldPosition - weaponShootPosition.position);
+
+        // Calculate direction vector of mouse cursor from player transform position
+        Vector3 playerDirection = (mouseWorldPosition - transform.position);
+
+        // Get weapon to cursor angle
+        weaponAngleDegrees = HelperUtilities.GetAngleInDegreesFromVector(weaponDirection);
+
+        // Get player to cursor angle
+        playerAngleDegrees = HelperUtilities.GetAngleInDegreesFromVector(playerDirection);
+
+        // Set player aim direction
+        playerAimDirection = HelperUtilities.GetAimDirection(playerAngleDegrees);
+
+        //Trigger weappon aim event
+        player.aimWeaponEvent.CallAimWeaponEvent(playerAimDirection, playerAngleDegrees, weaponAngleDegrees, weaponDirection);
+    }
+}
