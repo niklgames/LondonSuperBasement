798a943fae90fe0e2e43e321a592e785597ef220
Chris Williams <chris0n0williams@gmail.com>
2024-04-11 12:14:31 +0100

    Removed redundant space character from scriptable object path

--- Assets/Scripts/Player/CurrentPlayerSO.cs
+++ Assets/Scripts/Player/CurrentPlayerSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CurrentPlayer", menuName = "Scriptable Objects/Player/ Current Player")]
+[CreateAssetMenu(fileName = "CurrentPlayer", menuName = "Scriptable Objects/Player/Current Player")]
 public class CurrentPlayerSO : ScriptableObject
 {
     public PlayerDetailsSO playerDetails;
