9fc4b8ca8c259a5cf4e831ce5b8b8427d4272b76
Chris Williams <chris0n0williams@gmail.com>
2024-04-11 12:14:54 +0100

    Put a space character in scriptable object path

--- Assets/Scripts/Player/PlayerDetailsSO.cs
+++ Assets/Scripts/Player/PlayerDetailsSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerDetails_", menuName = "ScriptableObjects/Player/Player Details")]
+[CreateAssetMenu(fileName = "PlayerDetails_", menuName = "Scriptable Objects/Player/Player Details")]
 public class PlayerDetailsSO : ScriptableObject
 {
     #region Header PLAYER BASE DETAILS
