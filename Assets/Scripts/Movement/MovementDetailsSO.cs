69a5a2b05616cfb17c44bdc71e4576caf19ad3e8
Chris Williams <chris0n0williams@gmail.com>
2024-04-11 16:57:32 +0100

    Added dodge roll variables and validation to Movement details scriptable object

--- Assets/Scripts/Movement/MovementDetailsSO.cs
+++ Assets/Scripts/Movement/MovementDetailsSO.cs
@@ -18,6 +18,18 @@ public class MovementDetailsSO : ScriptableObject
     [Tooltip("The maximum move speed. The GetMoveSpeed method calculates a random value between the minimum and the maximum")]
     #endregion Tooltip
     public float maxMoveSpeed = 8f;
+    #region Tooltip
+    [Tooltip("If there is a roll movement - this is the roll speed")]
+    #endregion
+    public float rollSpeed; // for player
+    #region Tooltip
+    [Tooltip("If there is a roll movement - this is the roll distance")]
+    #endregion
+    public float rollDistance;
+    #region Tooltip
+    [Tooltip("If there is a roll movement - this is the cooldown time in seconds between roll actions")]
+    #endregion
+    public float rollCooldownTime; // for player
 
     /// <summary>
     /// Get a random movement speed between the minimum and maximum values
@@ -40,6 +52,13 @@ public class MovementDetailsSO : ScriptableObject
     private void OnValidate()
     {
         HelperUtilities.ValidateCheckPositiveRange(this, nameof(minMoveSpeed), minMoveSpeed, nameof(maxMoveSpeed), maxMoveSpeed, false);
+
+        if (rollDistance != 0f || rollSpeed !=0 || rollCooldownTime != 0)
+        {
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollDistance), rollDistance, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollSpeed), rollSpeed, false);
+            HelperUtilities.ValidateCheckPositiveValue(this, nameof(rollCooldownTime), rollCooldownTime, false);
+        }
     }
 
 #endif
